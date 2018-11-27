47bf1802aca59d760491d42216ddb1e7c711719a
skakun <krzysztof.michalski.it>
2018-11-27 03:34:39 +0100

    Crypt Decrypt overall test

--- src/test/java/app/CryptDecryptTest.java
+++ src/test/java/app/CryptDecryptTest.java
@@ -3,6 +3,7 @@ package app;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,11 +13,14 @@ public class CryptDecryptTest {
     {
         RSAKeyGenerator keygen=new RSAKeyGenerator();
         BigInteger[] key=keygen.generate();
+        BigInteger[]  publicKey= Arrays.copyOfRange(key, 0, 2)  ;
+        BigInteger[] privateKey=Arrays.copyOfRange(key, 1, 3);
         String  cryptogram="Ala ma kota !! 11 ą ";
         RSAEncryptor encryptor=new RSAEncryptor();
         RSADecryptor decryptor =new RSADecryptor();
-        BigInteger[] crypted=encryptor.encrypt(cryptogram,key);
-        String decrypted=decryptor.decrypt(crypted,key);
+
+        BigInteger[] crypted=encryptor.encrypt(cryptogram, privateKey);
+        String decrypted=decryptor.decrypt(crypted,publicKey);
         assertEquals(cryptogram,decrypted);
 
     }
